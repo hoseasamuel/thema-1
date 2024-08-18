3eb3a386ebdcc46c1317c701c684bee66318334c
gitfdil1248 <139831410+gitfdil1248@users.noreply.github.com>
2024-08-18 10:58:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,10 +63,7 @@ check_token() {
   read -r USER_TOKEN
 
   if [ "$USER_TOKEN" =
-     "f
-     ad
-     hostai
-     syah" ]; then
+     "fadhostaisyah" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke Fadhost${NC}"
